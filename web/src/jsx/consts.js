15082bd70ad552b8713255e19c3b46521e47e997
Aaron Spaulding <aaron@sachimp.com>
2023-06-23 21:02:52 -0400

    Fix typo in path.

--- web/src/jsx/consts.js
+++ web/src/jsx/consts.js
@@ -1,6 +1,6 @@
 import {MC_VERSION} from 'shared';
 
-export const DATA_DIR = `${ process.env.PUBLIC_URL ?? '' } /data/${ MC_VERSION }/`;
+export const DATA_DIR = `${ process.env.PUBLIC_URL ?? '' }/data/${ MC_VERSION }/`;
 const XYZ_SELECTION_RADIUS = 0.025;
 const LAB_SELECTION_RADIUS = 4;
 export const SELECTION_RADIUS = LAB_SELECTION_RADIUS;
