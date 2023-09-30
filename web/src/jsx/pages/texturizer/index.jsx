6e23dae890ea0d815e701bdb2d60a53b40fce4bb
Aaron Spaulding <aaron@sachimp.com>
2023-09-30 14:28:24 -0400

    Texturizer - add dithering

--- web/src/jsx/pages/texturizer/index.jsx
+++ web/src/jsx/pages/texturizer/index.jsx
@@ -35,6 +35,194 @@ function SwatchGrid({width, height, blocks}) {
 	</div>
 }
 
+function coordToIndex(width, x, y) {
+	return y * (width * 4) + x * 4;
+}
+
+
+/**
+ * @callback DitheringAlgo
+ * @param {ImageData} pixels
+ * @param {number} x
+ * @param {number} y
+ * @param {number} r
+ * @param {number} g
+ * @param {number} b
+ */
+
+const FLOYD_STEINBERG_7 = 7 / 16;
+const FLOYD_STEINBERG_5 = 5 / 16;
+const FLOYD_STEINBERG_3 = 3 / 16;
+const FLOYD_STEINBERG_1 = 1 / 16;
+
+const STUCKI_8 = 8 / 42;
+const STUCKI_4 = 4 / 42;
+const STUCKI_2 = 2 / 42;
+const STUCKI_1 = 1 / 42;
+
+const ATKINSON_1 = 1 / 8;
+
+/**
+ * @type {Record<string, DitheringAlgo | null>}
+ */
+const DITHERING_ALGOS = {
+	none: null,
+	floydSteinberg(pixels, x, y, r, g, b,) {
+		const width = pixels.width;
+		const nextRed = coordToIndex(width, x + 1, y);
+
+		pixels.data[nextRed] += r * FLOYD_STEINBERG_7;
+		pixels.data[nextRed + 1] += g * FLOYD_STEINBERG_7;
+		pixels.data[nextRed + 2] += b * FLOYD_STEINBERG_7;
+
+		const belowRed = coordToIndex(width, x, y + 1);
+
+		pixels.data[belowRed] += r * FLOYD_STEINBERG_5;
+		pixels.data[belowRed + 1] += g * FLOYD_STEINBERG_5;
+		pixels.data[belowRed + 2] += b * FLOYD_STEINBERG_5;
+
+		const belowNextRed = coordToIndex(width, x + 1, y + 1);
+
+		pixels.data[belowNextRed] += r * FLOYD_STEINBERG_1;
+		pixels.data[belowNextRed + 1] += g * FLOYD_STEINBERG_1;
+		pixels.data[belowNextRed + 2] += b * FLOYD_STEINBERG_1;
+
+		const belowPrevRed = coordToIndex(width, x - 1, y);
+
+		pixels.data[belowPrevRed] += r * FLOYD_STEINBERG_3;
+		pixels.data[belowPrevRed + 1] += g * FLOYD_STEINBERG_3;
+		pixels.data[belowPrevRed + 2] += b * FLOYD_STEINBERG_3;
+	},
+	// 'stucki': {
+	// 	'divisor': 42,
+	// 	'weights': [
+	// 		[0, 0, 0, 8, 4],
+	// 		[2, 4, 8, 4, 2],
+	// 		[1, 2, 4, 2, 1]
+	// 	]
+	// },
+	stucki(pixels, x, y, r, g, b) {
+		const width = pixels.width;
+		let pixel = coordToIndex(width, x + 1, y);
+
+		pixels.data[pixel] += r * STUCKI_8;
+		pixels.data[pixel + 1] += g * STUCKI_8;
+		pixels.data[pixel + 2] += b * STUCKI_8;
+
+		pixel = coordToIndex(width, x + 2, y);
+
+		pixels.data[pixel] += r * STUCKI_4;
+		pixels.data[pixel + 1] += g * STUCKI_4;
+		pixels.data[pixel + 2] += b * STUCKI_4;
+
+		pixel = coordToIndex(width, x - 2, y + 1);
+
+		pixels.data[pixel] += r * STUCKI_2;
+		pixels.data[pixel + 1] += g * STUCKI_2;
+		pixels.data[pixel + 2] += b * STUCKI_2;
+
+		pixel = coordToIndex(width, x - 1, y + 1);
+
+		pixels.data[pixel] += r * STUCKI_4;
+		pixels.data[pixel + 1] += g * STUCKI_4;
+		pixels.data[pixel + 2] += b * STUCKI_4;
+
+		pixel = coordToIndex(width, x, y + 1);
+
+		pixels.data[pixel] += r * STUCKI_8;
+		pixels.data[pixel + 1] += g * STUCKI_8;
+		pixels.data[pixel + 2] += b * STUCKI_8;
+
+		pixel = coordToIndex(width, x + 1, y + 1);
+
+		pixels.data[pixel] += r * STUCKI_4;
+		pixels.data[pixel + 1] += g * STUCKI_4;
+		pixels.data[pixel + 2] += b * STUCKI_4;
+
+		pixel = coordToIndex(width, x + 2, y + 1);
+
+		pixels.data[pixel] += r * STUCKI_2;
+		pixels.data[pixel + 1] += g * STUCKI_2;
+		pixels.data[pixel + 2] += b * STUCKI_2;
+
+		pixel = coordToIndex(width, x - 2, y + 2);
+
+		pixels.data[pixel] += r * STUCKI_1;
+		pixels.data[pixel + 1] += g * STUCKI_1;
+		pixels.data[pixel + 2] += b * STUCKI_1;
+
+		pixel = coordToIndex(width, x - 1, y + 2);
+
+		pixels.data[pixel] += r * STUCKI_2;
+		pixels.data[pixel + 1] += g * STUCKI_2;
+		pixels.data[pixel + 2] += b * STUCKI_2;
+
+		pixel = coordToIndex(width, x, y + 2);
+
+		pixels.data[pixel] += r * STUCKI_4;
+		pixels.data[pixel + 1] += g * STUCKI_4;
+		pixels.data[pixel + 2] += b * STUCKI_4;
+
+		pixel = coordToIndex(width, x + 1, y + 2);
+
+		pixels.data[pixel] += r * STUCKI_2;
+		pixels.data[pixel + 1] += g * STUCKI_2;
+		pixels.data[pixel + 2] += b * STUCKI_2;
+
+		pixel = coordToIndex(width, x + 2, y + 2);
+
+		pixels.data[pixel] += r * STUCKI_1;
+		pixels.data[pixel + 1] += g * STUCKI_1;
+		pixels.data[pixel + 2] += b * STUCKI_1;
+	},
+	// 'atkinson': {
+	// 	'divisor': 8,
+	// 	'weights': [
+	// 		[0, 0, 0, 1, 1],
+	// 		[0, 1, 1, 1, 0],
+	// 		[0, 0, 1, 0, 0]
+	// 	]
+	// }
+	atkinson(pixels, x, y, r, g, b) {
+		const width = pixels.width;
+		let pixel = coordToIndex(width, x + 1, y);
+
+		pixels.data[pixel] += r * ATKINSON_1;
+		pixels.data[pixel + 1] += g * ATKINSON_1;
+		pixels.data[pixel + 2] += b * ATKINSON_1;
+
+		pixel = coordToIndex(width, x + 2, y);
+
+		pixels.data[pixel] += r * ATKINSON_1;
+		pixels.data[pixel + 1] += g * ATKINSON_1;
+		pixels.data[pixel + 2] += b * ATKINSON_1;
+
+		pixel = coordToIndex(width, x - 1, y + 1);
+
+		pixels.data[pixel] += r * ATKINSON_1;
+		pixels.data[pixel + 1] += g * ATKINSON_1;
+		pixels.data[pixel + 2] += b * ATKINSON_1;
+
+		pixel = coordToIndex(width, x, y + 1);
+
+		pixels.data[pixel] += r * ATKINSON_1;
+		pixels.data[pixel + 1] += g * ATKINSON_1;
+		pixels.data[pixel + 2] += b * ATKINSON_1;
+
+		pixel = coordToIndex(width, x + 1, y + 1);
+
+		pixels.data[pixel] += r * ATKINSON_1;
+		pixels.data[pixel + 1] += g * ATKINSON_1;
+		pixels.data[pixel + 2] += b * ATKINSON_1;
+
+		pixel = coordToIndex(width, x, y + 2);
+
+		pixels.data[pixel] += r * ATKINSON_1;
+		pixels.data[pixel + 1] += g * ATKINSON_1;
+		pixels.data[pixel + 2] += b * ATKINSON_1;
+	}
+}
+
 export function Component() {
 	/** @type {React.MutableRefObject<HTMLCanvasElement>} */
 	const canvasRef = useRef(null);
@@ -43,6 +231,7 @@ export function Component() {
 	const [height, setHeight] = useState(DEFAULT_SIZE);
 	const [noiseScale, setNoiseScale] = useState(1);
 	const [textureBlocks, setTextureBlocks] = useState(null);
+	const [ditheringAlgo, setDitheringAlgo] = useState('stucki');
 
 	const [palette, setPalette] = useState('average');
 	const [helpOpen, setHelpOpen] = useState(false);
@@ -72,6 +261,7 @@ export function Component() {
 		const ctx = canvasRef.current.getContext('2d');
 		const pixels = ctx.getImageData(0, 0, width, height);
 		const gradient = new Gradient();
+		const ditheringMethod = DITHERING_ALGOS[ditheringAlgo];
 
 		gradient.addStop(0, RGBColor.fromInteger(0x33252e));
 		gradient.addStop(0.4437, RGBColor.fromInteger(0xff9b1b));
@@ -82,39 +272,70 @@ export function Component() {
 		// gradient.addStop(1, RGBColor.fromInteger(0x207d3d));
 
 
-		for (let x = 0; x < width; x++) {
-			for (let y = 0; y < width; y++) {
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
 				const noise = (noiserRef.current(x/noiseScale, y/noiseScale) / 2) + 0.5;
-				const red = y * (width * 4) + x * 4;
+				const red = coordToIndex(width, x, y);
 				const color = gradient.interpolate(noise).toRGBColor();
-
-				console.log(noise);
+				const block = blockLookup.find(color, palette).block;
+				const matchedColor = block.palette[palette].rgb;
 
 				pixels.data[red] = color.r;
 				pixels.data[red + 1] = color.g;
 				pixels.data[red + 2] = color.b;
 				pixels.data[red + 3] = 255;
+
+				const rDelta = color.r - matchedColor.r;
+				const gDelta = color.g - matchedColor.g;
+				const bDelta = color.b - matchedColor.b;
+
+
+				pixels[red + 4] = rDelta * (7 / 16);
+				pixels[red + 4 + 1] = gDelta * (7 / 16);
+				pixels[red + 4 + 2] = bDelta * (7 / 16);
 			}
 		}
 
-		const blocks = new Array(pixels.data.length / 4);
+		const color = new RGBColor(0, 0, 0);
+
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				const red = coordToIndex(width, x, y);
+
+				color.r = pixels.data[red];
+				color.g = pixels.data[red + 1];
+				color.b = pixels.data[red + 2]
+
+				const block = blockLookup.find(color, palette).block;
+				const matchedColor = block.palette[palette].rgb;
+
+				const rDelta = color.r - matchedColor.r;
+				const gDelta = color.g - matchedColor.g;
+				const bDelta = color.b - matchedColor.b;
+
+				if (ditheringMethod) {
+					ditheringMethod(pixels, x, y, rDelta, gDelta, bDelta);
+				}
+
+				pixels.data[red] = matchedColor.r;
+				pixels.data[red + 1] = matchedColor.g;
+				pixels.data[red + 2] = matchedColor.b;
+			}
+		}
+
+		const textureBlocks = new Array(pixels.data.length / 4);
 
 		for (let pixelIdx = 0; pixelIdx < pixels.data.length / 4; pixelIdx++) {
 			const red = pixelIdx * 4;
 			const blockColor = new RGBColor(pixels.data[red], pixels.data[red + 1], pixels.data[red + 2]);
 
-			blocks.push(blockLookup.find(blockColor, palette).block);
+			textureBlocks.push(blockLookup.find(blockColor, palette).block);
 		}
 
-		setTextureBlocks(blocks);
-
-		// pixels.data[0] = 255;
-		// pixels.data[1] = 0;
-		// pixels.data[2] = 0;
-		// pixels.data[3] = 255;
+		setTextureBlocks(textureBlocks);
 
 		ctx.putImageData(pixels, 0, 0);
-	}, [width, height, noiseScale, palette])
+	}, [width, height, noiseScale, ditheringAlgo, palette])
 
 	return <div className="page-texturizer">
 		<PaletteContext.Provider value={palette}>
@@ -129,7 +350,19 @@ export function Component() {
 				</label>
 				{/*<button onClick={() => setHelpOpen(true)}><FontAwesomeIcon icon={faQuestion} /></button>*/}
 			</AppTitleBar>
-			<input type="range" value={noiseScale} onInput={ (e) => setNoiseScale(+e.target.value) } min={1} max={ Math.max(width, height) * 2} />
+			<label>
+				Scale
+				<input type="range" value={noiseScale} onInput={ (e) => setNoiseScale(+e.target.value) } min={1} max={ Math.max(width, height) * 2} />
+			</label>
+			<label>
+				Dithering
+				<select value={ditheringAlgo} onInput={(e) => setDitheringAlgo(e.target.value)}>
+					<option value="none">None</option>
+					<option value="floydSteinberg">Floyd-Steinberg</option>
+					<option value="stucki">Stucki</option>
+					<option value="atkinson">Atkinson</option>
+				</select>
+			</label>
 			<canvas className={styles['texturizer-canvas']} ref={canvasRef} width={width} height={height} />
 			<SwatchGrid width={width} height={height} blocks={textureBlocks} />
 		</PaletteContext.Provider>
