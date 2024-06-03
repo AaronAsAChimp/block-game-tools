fb8df9d802e85396385c3f416f250dadbcff1bc8
Aaron Spaulding <aaron@sachimp.com>
2024-06-02 21:09:47 -0400

    Throw an error if the json files can't be found

--- web/src/jsx/index.jsx
+++ web/src/jsx/index.jsx
@@ -12,41 +12,43 @@ if (params.has('url')) {
     window.history.replaceState(null, null, decodeURIComponent(params.get('url')));
 }
 
-const router = createBrowserRouter([
-{
-  path: '/',
-  lazy: () => import('./pages/home/index.jsx')
-}, {
-  path: '/map/',
-  lazy: () => import('./pages/map/index.jsx'),
-  loader: () => {
-    return fetch(DATA_DIR + 'blocks.json');
-  }
-}, {
-  path: '/gradient/',
-  lazy: () => import('./pages/gradient/index.jsx'),
-  loader: () => {
-    return fetch(DATA_DIR + 'gradient-blocks.json');
-  }
-}, {
-  path: '/gradient/:colors',
-  lazy: () => import('./pages/gradient/index.jsx'),
-  loader: () => {
-    return fetch(DATA_DIR + 'gradient-blocks.json');
-  }
-}, {
-  path: '/texturizer',
-  lazy: () => import('./pages/texturizer/index.jsx'),
-  loader: () => {
-    return fetch(DATA_DIR + 'gradient-blocks.json');
-  }
-}, {
-  path: '/data/',
-  lazy: () => import('./pages/data/index.jsx'),
-  loader: () => {
-    return fetch(DATA_DIR + 'all-blocks.json');
+/**
+ * @param {string} url The URL of the JSON to load
+ */
+function loadJson(url) {
+  return async () => {
+    const res = await fetch(url);
+
+    // Doing it this way will assert that its actually JSON.
+    return await res.json();
   }
 }
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    lazy: () => import('./pages/home/index.jsx')
+  }, {
+    path: '/map/',
+    lazy: () => import('./pages/map/index.jsx'),
+    loader: loadJson(DATA_DIR + 'blocks.json')
+  }, {
+    path: '/gradient/',
+    lazy: () => import('./pages/gradient/index.jsx'),
+    loader: loadJson(DATA_DIR + 'gradient-blocks.json')
+  }, {
+    path: '/gradient/:colors',
+    lazy: () => import('./pages/gradient/index.jsx'),
+    loader: loadJson(DATA_DIR + 'gradient-blocks.json')
+  }, {
+    path: '/texturizer',
+    lazy: () => import('./pages/texturizer/index.jsx'),
+    loader: loadJson(DATA_DIR + 'gradient-blocks.json')
+  }, {
+    path: '/data/',
+    lazy: () => import('./pages/data/index.jsx'),
+    loader: loadJson(DATA_DIR + 'all-blocks.json')
+  }
 ], {
 basename: process.env.PUBLIC_URL ?? '/'
 });
