3b7cdd1711a8b098917a364c7127ea796f06bbf4
Aaron Spaulding <aaron@sachimp.com>
2024-09-09 20:45:58 -0400

    Bump Minecraft version to 1.21.1 even though there aren't any new textures in this version.

--- shared/index.js
+++ shared/index.js
@@ -1,6 +1,6 @@
 export {RGBColor, RGBAColor, LabColor, XYZColor, Color} from './src/color.js';
 
-export const MC_VERSION = '1.20.6';
+export const MC_VERSION = '1.21.1';
 
-// Can be looked up https://minecraft.fandom.com/wiki/Data_version
-export const MC_DATA_VERSION = 3839;
+// Can be looked up https://minecraft.wiki/w/Data_version
+export const MC_DATA_VERSION = 3955;
