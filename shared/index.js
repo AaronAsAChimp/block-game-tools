fdb500c2b4cc0a823869589d46682d7ff46f3a1d
Aaron Spaulding <aaron@sachimp.com>
2025-01-04 10:06:39 -0500

    Updates for Minecraft 1.21.4.

--- shared/index.js
+++ shared/index.js
@@ -1,6 +1,6 @@
 export {RGBColor, RGBAColor, LabColor, XYZColor, Color} from './src/color.js';
 
-export const MC_VERSION = '1.21.1';
+export const MC_VERSION = '1.21.4';
 
 // Can be looked up https://minecraft.wiki/w/Data_version
-export const MC_DATA_VERSION = 3955;
+export const MC_DATA_VERSION = 4189;
