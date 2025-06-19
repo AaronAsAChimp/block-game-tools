d9ad57621457f89aef4eb762f74b05d3f86d2a79
Aaron Spaulding <aaron@sachimp.com>
2025-06-19 13:58:18 -0400

    Updates for v1.21.6

--- shared/index.js
+++ shared/index.js
@@ -1,6 +1,6 @@
 export {RGBColor, RGBAColor, LabColor, XYZColor, Color} from './src/color.js';
 
-export const MC_VERSION = '1.21.4';
+export const MC_VERSION = '1.21.6';
 
 // Can be looked up https://minecraft.wiki/w/Data_version
-export const MC_DATA_VERSION = 4189;
+export const MC_DATA_VERSION = 4435;
