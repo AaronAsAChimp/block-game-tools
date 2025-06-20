6236f726d89b3879cb5ac63dad15c9f370392a19
Aaron Spaulding <aaron@sachimp.com>
2025-06-19 23:10:08 -0400

    Add sitemap.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://blockgametools.com/',
+  integrations: [
+    react(),
+    sitemap()
+  ],
   vite: {
     // build: {
     //   commonjsOptions: {
